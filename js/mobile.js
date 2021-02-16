f81a1e335d94654e6e6e1dbc7c3d012d6af09551
max <max.0x71@gmail.com>
2021-02-16 23:23:38 +0000

    made changes for more screen sizes

--- js/mobile.js
+++ js/mobile.js
@@ -1,12 +1,9 @@
 function detectMob() {
     const toMatch = [
         /Android/i,
-        /webOS/i,
         /iPhone/i,
         /iPad/i,
         /iPod/i,
-        /BlackBerry/i,
-        /Windows Phone/i
     ];
 
     return toMatch.some((toMatchItem) => {
